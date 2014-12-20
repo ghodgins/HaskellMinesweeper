961ef5ad961bcf6bb6b4efa40c71bab91c4d396f
finnci <4cfinn@gmail.com>
2014-12-20 11:35:07 +0000

    interface file, does not interact with anything else yet, just want people to be able to see what im doing

---
+++ src/interface.hs
@@ -0,0 +1,33 @@
+module Main where
+import Graphics.UI.WX
+
+w = 200
+h = 200
+main :: IO ()
+main
+  = start hello
+
+hello :: IO ()
+hello
+  = do   f <- frame [ text := "Minesweeper", clientSize := sz w h ]
+         p <- panel f []
+         st <- staticText p [ text := "Choose difficulty" ]
+         b <- button p [ text := "easy" , position := pt 5 20 ,on command := startgame  10]
+         b <- button p [ text := "medium" , position := pt 5 45 , on command := startgame  20]
+         b <- button p [ text := "hard" , position := pt 5 70 ,on command := startgame  30 ]
+
+         set p [clientSize := sz w h]
+
+
+startgame ::  Int -> IO ()
+startgame a 
+		= do  	 f <- frame [ text := "Minesweeper", clientSize := sz w h ]
+		         p <- panel f []
+		        -- st <- staticText p [ text := "lets go" ]
+		         b <- buttons a p
+		         set p [clientSize := sz w h]
+
+--need to change to return a list of buttons
+-- this method is to creat the x number of buttons based on level of difficulty
+buttons :: Int -> Panel () ->  IO (Button ())
+buttons a p = button p [text := "_", position := pt 10 10]
