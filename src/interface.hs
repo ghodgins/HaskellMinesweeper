b715564bfcc48f409557f4ae3dd4ecf1b19e8078
finnci <4cfinn@gmail.com>
2014-12-20 13:48:28 +0000

    comments

--- src/interface.hs
+++ src/interface.hs
@@ -1,6 +1,8 @@
 module Main where
 import Graphics.UI.WX
 
+--on Mac compile using: ghc --make interface.hs
+
 w = 200
 h = 200
 main :: IO ()
@@ -21,13 +23,14 @@ hello
 
 startgame ::  Int -> IO ()
 startgame a 
-		= do  	 f <- frame [ text := "Minesweeper", clientSize := sz w h ]
-		         p <- panel f []
-		        -- st <- staticText p [ text := "lets go" ]
-		         b <- buttons a p
-		         set p [clientSize := sz w h]
+    = do     f <- frame [ text := "Minesweeper", clientSize := sz w h ]
+             p <- panel f []
+            -- st <- staticText p [ text := "lets go" ]
+             b <- buttons a p
+             set p [clientSize := sz w h]
 
 --need to change to return a list of buttons
--- this method is to creat the x number of buttons based on level of difficulty
+-- this method is to create the x number of buttons based on level of difficulty.. currently creates 1 button
 buttons :: Int -> Panel () ->  IO (Button ())
+buttons 0 p = return                    --return once all buttons are created
 buttons a p = button p [text := "_", position := pt 10 10]
