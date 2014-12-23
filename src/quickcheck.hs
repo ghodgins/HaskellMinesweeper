ef84bae0019e0956a36dec7ef04fcdd25ba32dd3
finnci <4cfinn@gmail.com>
2014-12-23 01:00:53 +0000

    comments

--- src/quickcheck.hs
+++ src/quickcheck.hs
@@ -7,6 +7,7 @@ import Data.List
 
 
 --test that the grid will always be created the same
+--this test works
 prop_creategrid :: Int -> Int -> Bool
 prop_creategrid x y
 	= createGrid x y == createGrid x y
