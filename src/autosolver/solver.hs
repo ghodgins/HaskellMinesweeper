de39e49da5ee71b33ebcb59c0cbfee32c7e696b1
Geoffrey Hodgins <geoffrey.hodgins@gmail.com>
2014-12-23 14:22:05 -0800

    autosolver/solver commit

--- src/autosolver/solver.hs
+++ src/autosolver/solver.hs
@@ -1,4 +1,11 @@
+{-#LANGUAGE RecordWildCards #-}
+
+module Solver where
+
 import Minesweeper
+import Square
+import Types
+import Data.List
 
 {-
 1. traverse through every point on the map
@@ -6,17 +13,29 @@ import Minesweeper
 3. if it is, add the point to a list
 -}
 hiddenFrontier :: Game -> [Point]
-hiddenFrontier game@Game{..} = hiddenFrontier' (allPoints game (((length board)*(length (board!!0))))) $ concat board
+hiddenFrontier game@Game{..} = hiddenFrontier' (allPoints game (((length board)*(length (board!!0)))) []) board
 
-hiddenFrontier' :: [Point] -> [Square] -> [Point]
-hiddenFrontier' points board = filter (is points board)
+hiddenFrontier' :: [Point] -> [[Square]] -> [Point]
+hiddenFrontier' [] _ = []
+hiddenFrontier' (x:xs) board = case isHidden (getSquare board x) && 
+                                    isBesideVisible (getSquares board (adjacentSquares x (length board) (length (board!!0)))) of
+    True  -> x : (hiddenFrontier' xs board)
+    False -> hiddenFrontier' xs board
 
 isHidden :: Square -> Bool
-isHidden square = if square == (HiddenNumSquare _)
-                      then True
-                      else False
+isHidden square = case square of
+    (HiddenNumSquare _)  -> True
+    _                    -> False
 
-besideVisible :: Square -> [Square] -> Bool
-besideVisible square surrounding = any (\x -> x == (VisibleNumSquare _)) surrounding
+isBesideVisible :: [Square] -> Bool
+isBesideVisible surrounding = any (\x -> isVisible x) surrounding
+
+isVisible :: Square -> Bool
+isVisible square = case square of
+    (VisibleNumSquare _) -> True
+    _                    -> False
 
 -- returns the numbered squares next to unexplored fields
+-- gets hiddenFrontier and finds complement set of it and all the points
+visibleFrontier :: Game -> [Point]
+visibleFrontier game@Game{..} = (allPoints game (((length board)*(length (board!!0)))) []) \\ hiddenFrontier game
